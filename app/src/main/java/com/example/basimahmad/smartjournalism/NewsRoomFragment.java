2ba65c653d266a44552de1c4c0f358c511a3d955
Basim Ahmad <basimahmad05@gmail.com>
2017-12-10 19:27:30 +0500

    Done with Inbox first page

--- app/src/main/java/com/example/basimahmad/smartjournalism/NewsRoomFragment.java
+++ app/src/main/java/com/example/basimahmad/smartjournalism/NewsRoomFragment.java
@@ -12,35 +12,49 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.Volley;
+import com.example.basimahmad.smartjournalism.R;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import android.widget.AdapterView.OnItemSelectedListener;
+import com.example.basimahmad.smartjournalism.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import mehdi.sakout.fancybuttons.FancyButton;
 
 public class NewsRoomFragment extends Fragment{
+    private static final String URL_Categories = "http://www.krunchycorner.net/categories.php";
+
     View view;
     ArrayList<String> file_names = new ArrayList<String>();
     Button attachment;
     FancyButton publish;
+    private Spinner category;
     EditText _title, _subject, _description;
     private static final String TAG = "NewsRoomFragment";
     private ProgressDialog pDialog;
     private SessionManager session;
-
+    ArrayList<String> bankNames1= new ArrayList<String>();
+    String[] bankNames = {"Politics", "Entertainment", "Sports", "Weather", "Religion"};
+    String _category = "Politics";
     public NewsRoomFragment() {
         // Required empty public constructor
     }
@@ -61,19 +75,40 @@ public class NewsRoomFragment extends Fragment{
         _title = (EditText) view.findViewById(R.id.input_title);
         _subject = (EditText) view.findViewById(R.id.input_subject);
         _description = (EditText) view.findViewById(R.id.input_des);
+        category = (Spinner) view.findViewById(R.id.category);
 
         session = new SessionManager(getActivity());
 
         // Progress dialog
         pDialog = new ProgressDialog(getActivity());
         pDialog.setCancelable(false);
-
+        //loadCategories();
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter aa = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_item,bankNames);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        category.setAdapter(aa);
         publish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 publishNews();
             }
         });
+
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Toast.makeText(getActivity(), bankNames[position], Toast.LENGTH_LONG).show();
+                _category = bankNames[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
 
         attachment.setOnClickListener(new View.OnClickListener() {
@@ -90,6 +125,7 @@ public class NewsRoomFragment extends Fragment{
 
         return view;
     }
+
 
     public void publishNews() {
         Log.d(TAG, "publishNews");
@@ -174,12 +210,14 @@ public class NewsRoomFragment extends Fragment{
                 if (file_names.size() == 0){
                     params.put("uid", String.valueOf(session.getUserID()));
                     params.put("title", title);
+                    params.put("category", _category);
                     params.put("subject", subject);
                     params.put("description", desc);
                 }
                 if (file_names.size() == 1){
                    params.put("uid", String.valueOf(session.getUserID()));
                     params.put("title", title);
+                    params.put("category", _category);
                     params.put("subject", subject);
                     params.put("description", desc);
                     params.put("file1", file_names.get(0));
@@ -187,6 +225,7 @@ public class NewsRoomFragment extends Fragment{
                 if (file_names.size() == 2){
                     params.put("uid", String.valueOf(session.getUserID()));
                     params.put("title", title);
+                    params.put("category", _category);
                     params.put("subject", subject);
                     params.put("description", desc);
                     params.put("file1", file_names.get(0));
@@ -195,6 +234,7 @@ public class NewsRoomFragment extends Fragment{
                 if (file_names.size() == 3){
                     params.put("uid", String.valueOf(session.getUserID()));
                     params.put("title", title);
+                    params.put("category", _category);
                     params.put("subject", subject);
                     params.put("description", desc);
                     params.put("file1", file_names.get(0));
@@ -219,5 +259,51 @@ public class NewsRoomFragment extends Fragment{
     private void hideDialog() {
         if (pDialog.isShowing())
             pDialog.dismiss();
+    }
+
+    private void loadCategories() {
+
+        /*
+        * Creating a String Request
+        * The request type is GET defined by first parameter
+        * The URL is defined in the second parameter
+        * Then we have a Response Listener and a Error Listener
+        * In response listener we will get the JSON response as a String
+        * */
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_Categories,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting the string to json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //traversing through all the object
+                            for (int i = 0; i < array.length(); i++) {
+
+                                //getting product object from json array
+                                JSONObject product = array.getJSONObject(i);
+
+                                //adding the product to product list
+                                bankNames1.add(
+                                        product.getString("category")
+                                );
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        //adding our stringrequest to queue
+        Volley.newRequestQueue(getActivity()).add(stringRequest);
     }
 }
